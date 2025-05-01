fea56b096fec7583cba09af258a31a3ea46ccd82
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-05-01 14:54:57 -0500

    Estilos navbar y footer general de la pagina

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import logo from "../assets/logo.png";
+
+function Footer() {
+  return (
+    <footer className="bg-primary text-white text-center py-4">
+      <img src={logo} alt="Logo" width="50" />
+      <h5>Desarrollado por Miguel Ortiz</h5>
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import logo from "../assets/logo.png";
+
+function Navbar() {
+  return (
+    <header className="bg-primary text-white py-3">
+      <nav className="navbar navbar-expand-lg bg-primary navbar-dark">
+        <div className="container">
+          <a href="/" className="navbar-brand">
+            <img
+              src={logo}
+              alt="MarketCali Logo"
+              className="me-2 img-thumbnail"
+            />
+            MarketCali
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarNav"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a href="/productos" className="nav-link">
+                  Productos
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/contacto" className="nav-link">
+                  Contacto
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link">
+                  Iniciar Sesión
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
