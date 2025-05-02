7cbd1ad2cf22a08923cf7717e0e79daf676b2a49
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-05-02 00:26:26 -0500

    Mejorar footer con diseño

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,31 @@
 import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="bg-primary text-white text-center py-4">
-      <img src={logo} alt="Logo" width="50" />
-      <h5>Desarrollado por Miguel Ortiz</h5>
+    <footer className="bg-primary text-white pt-4 mt-5">
+      <div className="container">
+        <div className="row align-items-center text-center text-md-start">
+          {/* Logo + nombre */}
+          <div className="col-md-4 mb-3 mb-md-0 d-flex align-items-center justify-content-center justify-content-md-start">
+            <img src={logo} alt="MarketCali Logo" width="50" className="me-2" />
+            <div>
+              <h5 className="mb-0">MarketCali</h5>
+              <small>Desarrollado por Miguel Ortiz</small>
+            </div>
+          </div>
+
+          {/* Derechos */}
+          <div className="col-md-4 text-center text-md-end">
+            <p className="mb-0 small">
+              &copy; {new Date().getFullYear()} MarketCali. Todos los derechos
+              reservados.
+            </p>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
+
 export default Footer;
